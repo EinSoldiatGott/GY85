538f7606d1771e7bc8d7c2f3aebc3b6308504be8
Ein-PC <EinSoldiatGott@gmail.com>
2015-07-06 02:27:23 -0500

    Ganancia 0.8
    Signed-off-by: Ein-PC <EinSoldiatGott@gmail.com>

--- HMC.c
+++ HMC.c
@@ -21,6 +21,23 @@ bool verificaComHMC(){
 
 }
 
+void configHMC(){
+	registro_I2C = reg_cfA;
+	valor_I2C=0x70;					//8 promedios por lectura + 15Hz + polarización normal
+	escribeI2C(dir_HMC,2);
+	delay_ms(1);
+
+	registro_I2C = reg_cfB;
+	valor_I2C=0x40;					//Ganancia 1.9Gauss
+	valor_I2C=0x20;					//Ganancia 1.3Gauss
+	valor_I2C=0x00;					//Ganancia 0.8Gauss
+	escribeI2C(dir_HMC,2);
+
+
+//	leeI2C(dir_HMC,6);
+}
+
+
 void leeHMCxyz(){
 	registro_I2C = reg_Mod;
 	valor_I2C=mod_continuo;
